be4449f5aae46520f73f867007c197f5c60ce5ce
Joshua Boothe <boo22005@byui.edu>
2025-04-26 22:40:24 -0600

    Prep1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Please input your first name");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("Please input your last name");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
-}
+    }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage for this class?");
+        string valueInText = Console.ReadLine();
+        int numberGrade = int.Parse(valueInText);
+        string letter = "xx";
+        if (numberGrade >= 90){
+            letter = "A";
+        }
+        else if (numberGrade >= 80){
+            letter = "B";
+        }
+        else if (numberGrade >= 70){
+            letter = "C";
+        }
+        else if (numberGrade >= 60){
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is a(n) {letter}");
+        if (numberGrade >=70){
+            Console.WriteLine($"You passed the class!");
+        }
+        else {
+            Console.WriteLine($"You failed the class, better luck next time!");
+        }
     }
 }
