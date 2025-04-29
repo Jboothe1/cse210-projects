5fe0ab72d34c3b7b94547456ee4fb216a45cf38f
Joshua Boothe <boo22005@byui.edu>
2025-04-29 09:36:10 -0600

    Sandbox For Loop

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Sandbox World!");
-        Console.WriteLine("Please input your name");
-        string firstname = Console.ReadLine();
-        Console.WriteLine($"Your name is: {firstname}");
+    //     // Console.WriteLine("Hello Sandbox World!");
+    //     Console.WriteLine("Please input your name");
+    //     string firstname = Console.ReadLine();
+    //     Console.WriteLine($"Your name is: {firstname}");
+    // }
+    int x = 10;
+    // if (x==10)
+    // {
+    //     Console.WriteLine("x is 10");
+    // }
+    for(int i = 0; i < x; i++)
+    {
+        Console.WriteLine($"Peanut Butter {i}");
     }
     }
+}
