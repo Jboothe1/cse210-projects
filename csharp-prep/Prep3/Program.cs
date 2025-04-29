014d5a7f9e3b5e40b96a986eca82f6e6e8b8834e
Joshua Boothe <boo22005@byui.edu>
2025-04-29 14:52:52 -0600

    Prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    static void Main(string[] args){
+
+      int x = 18;
+      int NumberGuess = 0;
+
+    while(NumberGuess != 18){
+         Console.WriteLine("Guess the magic number: ");
+         Random randomGenerator = new Random();
+         NumberGuess = randomGenerator.Next(1, 100);
+         Console.WriteLine($"Your guess was {NumberGuess}");
+         // string valueInText = Console.ReadLine();
+         // NumberGuess = int.Parse(valueInText);
+         if (NumberGuess > x){
+            Console.WriteLine("The magic number is smaller. Try again");
+         }
+         if (NumberGuess < x){
+            Console.WriteLine("The magic number is larger. Try again");
+         }
+         if (NumberGuess == 18){
+            Console.WriteLine("You guessed the magic number!");
+          }
+    }
+
+       
+    
     }
 }
